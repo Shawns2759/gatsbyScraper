10def7cfe8c63663fba5248437780d331a1927f4
Shawn Schneider <shawns2759@gmail.com>
2021-09-29 17:31:13 -0700

    changed header component to fix build issue

--- src/components/header.js
+++ src/components/header.js
@@ -2,17 +2,14 @@ import React from 'react'
 import { Link } from "gatsby"
 import styles from '../styles/global.css'
 
+
 export default function Header() {
     return (
-        <>
+        <div>
             <span className="flex m-1 p-2 border-b-2 border-solid border-gray-300 mb-10 justify-between ">
             <Link to="/">PalouseTechJobs</Link>
-                <Link to="/">Home</Link>
+            <Link to="/">Home</Link>
             </span>
-        </>
+        </div>
     )
 }
-
-{/* <Link to="/work/meter">Meter</Link>
-                <Link to="/work/emsi">Emsi</Link>
-                <Link to="/work/redballoon">Redballoon</Link> */}
