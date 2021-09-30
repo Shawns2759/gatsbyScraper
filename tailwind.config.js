5bfbc618c3a7a2797fd7e7bd66fb4d9ac12817e2
Shawn Schneider <shawns2759@gmail.com>
2021-09-29 17:24:03 -0700

    fixing tailwind config to deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,12 @@
 module.exports = {
   purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {
-      colors: {
-        btnBlue:{
-    500:"#4979ff"
-        }, 
-        hover: {
-          500:"#FFDE33"
-        }
-      }
-    },
-  },
-  plugins: [],
-}
+  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+   darkMode: false, // or 'media' or 'class'
+   theme: {
+     extend: {},
+   },
+   variants: {
+     extend: {},
+   },
+   plugins: [],
+ }
